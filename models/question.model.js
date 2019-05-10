0f60f880c14bfa60415ce0b5b200d9ef19e6320d
Abbey <i.olasunkanmi@dreammesh.ng>
2019-05-10 16:51:02 +0100

    Created questions model

---
+++ models/question.model.js
@@ -0,0 +1,29 @@
+let questions = require("../data/questions.json");
+const helper = require("../helpers/helper.js");
+
+function getQuestions() {
+  return new Promise((resolve, reject) => {
+    if (questions.length === 0) {
+      reject({
+        message: "no questions available",
+        status: 202
+      });
+    }
+
+    resolve(questions);
+  });
+}
+
+function getQuestion(id) {
+  return new Promise((resolve, reject) => {
+    helper
+      .findAQuestion(questions, id)
+      .then(question => resolve(question))
+      .catch(err => reject(err));
+  });
+}
+
+module.exports = {
+  getQuestions,
+  getQuestion
+};
