01c46513ed77d930cdc669c878e13ed6b005f743
Abbey <i.olasunkanmi@dreammesh.ng>
2019-05-10 17:43:26 +0100

    Updated the package.json and index.js files

--- index.js
+++ index.js
@@ -14,4 +14,6 @@ app.get("/", (req, res) => {
 });
 
 // Starting server
-app.listen("5000");
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
