e2c3667ec037286fd4ced15e833334330f820a0f
Abbey <i.olasunkanmi@dreammesh.ng>
2019-05-12 14:47:02 +0100

    Made adjustment on morgan dependency

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.get("/", (req, res) => {
 });
 
 // Starting server
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
